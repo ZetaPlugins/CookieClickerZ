12b71c4a03d994d12db97dcda164960823c4d769
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2025-04-30 19:26:26 +0200

    Fixed achievement gui click check

--- src/main/java/org/strassburger/cookieclickerz/listeners/inventory/AchievementsGuiClickListener.java
+++ src/main/java/org/strassburger/cookieclickerz/listeners/inventory/AchievementsGuiClickListener.java
@@ -1,12 +1,18 @@
 package org.strassburger.cookieclickerz.listeners.inventory;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.strassburger.cookieclickerz.util.gui.AchievementGUI;
 
 public class AchievementsGuiClickListener implements Listener {
     @EventHandler
     public void onClickEvent(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
+
+        if (!AchievementGUI.isOpen(player)) return;
+
         event.setCancelled(true);
     }
 }
